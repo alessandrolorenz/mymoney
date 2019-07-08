105d7d514687149dd63bb67e237e1ca6b587edb8
alessandrocunhalorenz <alessandro.lorenz@gmail.com>
2019-07-08 17:45:04 -0300

    Primeiro commit- fim da parte backend

---
+++ my-money-app/backend/src/config/cors.js
@@ -0,0 +1,6 @@
+module.exports = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next() //middleware - responder ou dar a msn erro ou next para o proximo middleware - 
+}
